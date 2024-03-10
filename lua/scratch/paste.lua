4da0bffa55e40772ab7fda7316edb6cda39ee26a
dharmx <dharmx.dev@gmail.com>
2024-03-11 01:16:12 +0530

    feat: updated scratch/paste

--- lua/scratch/paste.lua
+++ lua/scratch/paste.lua
@@ -6,36 +6,31 @@ local Job = require("plenary.job")
 M._responses = {}
 M.config = {
   db_path = vim.fn.stdpath("state") .. "/paste.db.json",
-  tmp_path = "/tmp/paste.txt",
+  tmp_path = "/tmp/paste",
+  dump_path = "/tmp/dump",
 }
 
 function M.setup(options)
   options = vim.F.if_nil(options, {})
   M.config = vim.tbl_deep_extend("keep", options, M.config)
   M._db = Path:new(M.config.db_path)
+  M._dump = Path:new(M.config.dump_path)
   M._responses = {}
-  if M._db:exists() then
-    M._responses = vim.json.decode(M._db:read())
-  end
+  if M._db:exists() then M._responses = vim.json.decode(M._db:read()) end
 end
 
 function M.delete(response)
-  local token
-  for _, header in ipairs(response.headers) do
-    if header:match("^x%-token: ") then token = header end
-  end
   Job:new({
     command = "curl",
     args = {
-      "-Ftoken=" .. vim.split(token, ": ", { plain = true })[2],
-      "-Fdelete= ",
+      "--form",
+      string.format("token=%s", response.headers["x-token"]),
+      "--form",
+      "delete= ",
       response.body,
     },
     on_exit = vim.schedule_wrap(function(self, code, _)
-      if code ~= 0 then
-        vim.api.nvim_notify(vim.inspect(self:stderr_result()), vim.log.levels.INFO, { title = "pastebin", icon = " " })
-        return
-      end
+      if code ~= 0 then return end
       vim.api.nvim_notify(vim.inspect(self:result()), vim.log.levels.INFO, { title = "pastebin", icon = " " })
       M._responses[response.body] = nil
       M._db:write(vim.json.encode(M._responses), "w")
@@ -43,24 +38,57 @@ function M.delete(response)
   }):start()
 end
 
+function M.pretty(data)
+  local lines = vim.split(data, "\r\n", { plain = true })
+  local headers = {}
+  local heading = vim.split(lines[1], " ", { plain = true })
+  local code = heading[2]
+  table.remove(lines, 1)
+  for _, line in ipairs(lines) do
+    local _line = vim.split(line, ": ", { plain = true })
+    if #_line > 1 then
+      local _colon_items = vim.split(_line[2], "; ", { plain = true })
+      if #_colon_items == 1 then
+        headers[_line[1]] = _colon_items[1]
+      elseif #_colon_items > 1 then
+        headers[_line[1]] = _colon_items
+      end
+    end
+  end
+  return { code = code, headers = headers }
+end
+
 function M.paste(name, contents)
   if name == "" then name = M.config.tmp_path else name = string.format("/tmp/%s", name) end
   local path = Path:new(name)
   path:touch()
   path:write(table.concat(contents, "\n"), "w")
-  P(curl.post("https://0x0.st", {
-    dry_run = true,
-    form = { file = string.format("@%s", path.filename) },
-    callback = function(response)
-      M._responses[response.body] = response
+  Job:new({
+    command = "curl",
+    args = {
+      "--silent",
+     "--show-error",
+     "--location",
+     "--dump-header",
+      M._dump.filename,
+     "--compressed",
+     "--request",
+     "POST",
+     "--form",
+      string.format("file=@%s", path.filename),
+     "https://0x0.st",
+    },
+    on_exit = vim.schedule_wrap(function(self, code, _)
+      if code ~= 0 then return end
+      local body = self:result()[1]
+      local pretty = M.pretty(M._dump:read())
+      M._responses[body] = { body = body, headers = pretty.headers, code = pretty.code }
       path:rm()
-      vim.schedule(function()
-        vim.fn.setreg("+", response.body)
-        vim.api.nvim_notify("+pastebin: " .. response.body, vim.log.levels.INFO, { title = "pastebin", icon = " " })
-        M._db:write(vim.json.encode(M._responses), "w")
-      end)
-    end,
-  }))
+      vim.fn.setreg("+", body)
+      vim.api.nvim_notify("+pastebin: " .. body, vim.log.levels.INFO, { title = "pastebin", icon = " " })
+      M._db:write(vim.json.encode(M._responses), "w")
+    end),
+  }):start()
 end
 
 function M.command(args)
